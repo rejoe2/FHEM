a6028c62ce231c90fac2cae8e81045813f954b7c
rejoe2 <rejoe2@users.noreply.github.com>
2022-05-23 15:04:50 +0200

    Add files via upload

--- 10_RHASSPY.pm
+++ 10_RHASSPY.pm
@@ -2291,8 +2291,7 @@ sub getDevicesByGroup {
     if ( $hash->{experimental} ) {
         my $intent = $data->{intent} // return;
         $intent =~ s{Group\z}{}x;
-        my $single = getDeviceByName( $hash, $room, $data->{Group}, $data->{Room}, $intent, $intent ) // return;
-        my $spcls = $hash->{helper}{devicemap}{devices}{$single}{group_specials};
+        my $single = getDeviceByName( $hash, $room, $data->{Group}, $data->{Room}, $intent, $intent );return if !$single || ref $single eq 'ARRAY';
         $devices->{$single} = { delay => 0, prio => 0 };
     }
 
